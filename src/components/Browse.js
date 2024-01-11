0732120f5c03ae9be1a48a9e3c316ceb934df7e3
rahulsbeelur <srahulbeelur@gmail.com>
2024-01-11 17:38:51 +0530

    TMDB integration

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,6 +1,25 @@
 import Header from './Header';
+import { API_OPTIONS } from '../utils/constants';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { addNowPlayingMovies } from '../utils/moviesSlice';
 
 const Browse = () => {
+    const dispatch = useDispatch();
+
+    const getNowPlayingMovies = async () => {
+        const data = await fetch(
+            'https://api.themoviedb.org/3/movie/now_playing?page=1',
+            API_OPTIONS
+        );
+        const json = await data.json();
+        console.log(json);
+        dispatch(addNowPlayingMovies(json.results));
+    };
+
+    useEffect(() => {
+        getNowPlayingMovies();
+    }, []);
     return (
         <div>
             <Header />
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,3 +4,12 @@ export const loginImage =
     'https://assets.nflxext.com/ffe/siteui/vlv3/c38a2d52-138e-48a3-ab68-36787ece46b3/eeb03fc9-99c6-438e-824d-32917ce55783/IN-en-20240101-popsignuptwoweeks-perspective_alpha_website_large.jpg';
 export const userLogo =
     'https://occ-0-1492-3663.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABVB2PLirdQh_1yd9FQBW3HBGcUmWVlTDrUt5l1W_lqDJ33Sl-F6vY8O2X_URorArVYCkG0SnXjA6LXqHBLFVMg_DMtx19TA.png?r=cf8';
+
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization:
+            'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYjdlYzI2ZjVkN2ZhMTZkNjk2YjFjY2JiZjkxOTZjNCIsInN1YiI6IjY1OWZhODEzOTA3ZjI2MDEyYjIwZTJhYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.pev8faKTeQA7madBq7QdDvPd4zMyTYwT0rrM-rSlCXc'
+    }
+};
